e5420c090a4a5a6112a47e2ac8cac6400d401446
codemasternode <marcinwarzybok@outlook.com>
2019-07-14 02:34:20 +0200

    add small animation

--- client/src/components/sections/CategoryList.js
+++ client/src/components/sections/CategoryList.js
@@ -1,13 +1,43 @@
 import React from "react";
-import { ListGroup, Col, Container } from "react-bootstrap";
 
 export default ({ categories }) => {
+  const [state, setState] = React.useState({
+    leftLinePosition: 0,
+    isVisible: false,
+    widthLine: 0
+  });
+
   return (
     <div className="category-list">
-      <div className="category-container">
+      <div
+        className="category-container"
+        onMouseLeave={() => {
+          setState({ ...state, isVisible: false, widthLine: 0 });
+        }}
+      >
+        <div
+          className="line"
+          style={{
+            left: state.leftLinePosition,
+            width: state.widthLine,
+            opacity: state.isVisible ? 1 : 0
+          }}
+        />
         {categories.map((value, index) => {
-          console.log(value);
-          return <div className="category-list__item">{value.name}</div>;
+          return (
+            <div
+              className="category-list__item"
+              onMouseEnter={e => {
+                setState({
+                  widthLine: e.target.getBoundingClientRect().width,
+                  leftLinePosition: e.target.offsetLeft,
+                  isVisible: true
+                });
+              }}
+            >
+              {value.name}
+            </div>
+          );
         })}
       </div>
     </div>
